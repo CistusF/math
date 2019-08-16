f4704d32fb8b82e51190eef16f23f4e6228ebb5c
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2019-08-16 18:39:55 +0900

    Create 비교.js

---
+++ 비교.js
@@ -0,0 +1,12 @@
+var A = 7000
+var B = 10000
+
+if (A > B) {
+    console.log('A가 B보다 크다')
+} else if (A < B) {
+    console.log('A는 B보다 작다')
+} else if (A == B) {
+    console.log('A는 B와 같다')
+} else {
+    console.log('오류')
+}
