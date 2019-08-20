b874d720f6157829be8cf5a7193031719823d287
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2019-08-20 15:30:36 +0900

    Update 집합.js

--- 집합.js
+++ 집합.js
@@ -1,22 +1,55 @@
 var A = ['1', '2', '3']
-var B = ['1', '2', '3', '4']
+var B = ['1', '2', '5']
 
 var result = 0
-for (i = 0; i < B.length; i++) {
-    if (A.includes(B[i])) {
-        result++
+if (typeof A == 'object') {
+    if (typeof B == 'object') {
+        for (i = 0; i < B.length; i++) {
+            if (A.includes(B[i])) {
+                result++
+            }
+        }
+    } else {
+        for (i = 0; i < A.length; i++) {
+            if (B.includes(A[i])) {
+                result++
+            }
+        }
+    }
+} else {
+    if (typeof B == 'object') {
+        for (i = 0; i < B.length; i++) {
+            if (A.includes(B[i])) {
+                result++
+            }
+        }
+    } else {
+        if (A == B) {
+            result++
+        }
     }
 }
-if (result == B.length || result == A.length) {
-    if (A.length == B.length) {
-        console.log('A = B')
-    } else if (A.length > B.length) {
-        console.log('A는 B를 포함한다')
+
+if (typeof A == 'object' && typeof B == 'object') {
+    if (result == B.length || result == A.length) {
+        if (A.length == B.length) {
+            console.log('A = B')
+            console.log('A는 B를 포함한다')
+            console.log('B는 A를 포함한다')
+        } else if (A.length > B.length) {
+            console.log('A는 B를 포함한다')
+        } else {
+            console.log('B는 A를 포함한다')
+        }
+    } else if (result < B.length || result < A.length) {
+        console.log('교집합')
     } else {
-        console.log('B는 A를 포함한다')
+        console.log('여집합')
     }
-} else if (result < B.length) {
-    console.log('교집합')
 } else {
-    console.log('여집합')
+    if (A == B) {
+        console.log('A = B')
+    } else {
+        console.log('여집합')
+    }
 }
